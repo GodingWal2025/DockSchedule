dda89c4184ccb1941b7b863883ee3ce352400ccb
GodingWal2025 <GodingWal2025@users.noreply.github.com>
2026-06-24 09:49:32 -0500

    Auto-convert ADO.NET User ID and Password fields to standard ODBC UID and PWD format

--- api/function_app.py
+++ api/function_app.py
@@ -57,6 +57,8 @@ class DatabaseConnection:
             conn_str = conn_str.replace("encrypt=True", "encrypt=yes").replace("encrypt=False", "encrypt=no")
             conn_str = conn_str.replace("TrustServerCertificate=True", "TrustServerCertificate=yes").replace("TrustServerCertificate=False", "TrustServerCertificate=no")
             conn_str = conn_str.replace("trustServerCertificate=True", "trustServerCertificate=yes").replace("trustServerCertificate=False", "trustServerCertificate=no")
+            conn_str = conn_str.replace("User ID=", "UID=").replace("user id=", "uid=").replace("User ID =", "UID=")
+            conn_str = conn_str.replace("Password=", "PWD=").replace("password=", "pwd=").replace("Password =", "PWD=")
             if "Driver=" not in conn_str:
                 try:
                     # Try Driver 18 first
